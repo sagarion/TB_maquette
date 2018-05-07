9193337abfdad4a943f835bc24ddecdfabc8be2d
sagarion <thibault.daucourt@he-arc.ch>
2018-05-07 11:31:25 +0200

    Create maquette1Resume.php

---
+++ maquette1Resume.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<form>
+  <p>Bienvenu le prix de la livraison était de <?php echo $_GET["prix"]; ?> CHF</p>
+  <p>vous avez fourni le justificatif suivant:
+    </br>
+    <img src="http://img.over-blog-kiwi.com/1/70/62/34/20150820/ob_1e5159_img-20150819-215526939.jpg" alt="ticket_justificatif" height="100" width="100">
+  </p>
+  <p> Voulez-vous valider ces informations ?</p>
+  <button action="">valider<button><button action="https://sagarion.github.io/TBmaquette/maquette1.htm">retour<button>
+
+</form>
+</body>
+</html>
